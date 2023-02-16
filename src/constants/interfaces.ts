2bbe5596e8916ec99d0179ff154e649e9e50af19
Frolikss <darkness1198@gmail.com>
2023-02-16 11:36:32 +0200

    added ts & refactor form folder

---
+++ src/constants/interfaces.ts
@@ -0,0 +1,19 @@
+import { InputProps } from '@form';
+import { FC } from 'react';
+
+export interface Field {
+  name: string;
+  component: FC<InputProps>;
+  type: string;
+  placeholder: string;
+  variant: string;
+  validation: {
+    required: boolean;
+    pattern: {
+      value: RegExp;
+      message: string;
+    };
+  };
+}
+
+export type FieldData = Omit<Field, 'component'>;
